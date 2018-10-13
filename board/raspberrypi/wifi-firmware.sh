60f9112773b48f733ea908b361934f10de15f63d
ounie_os <ounie@qq.com>
2018-10-13 10:02:49 +0800

    wifi驱动使用ko加载

--- board/raspberrypi/wifi-firmware.sh
+++ board/raspberrypi/wifi-firmware.sh
@@ -5,7 +5,9 @@ set -e
 
 BOARD_DIR="$(dirname $0)"
 WIFI_FIRMWARE="${BOARD_DIR}/brcm"
+WIFI_SCRIPT="${BOARD_DIR}/S99wifi-module"
 
 mkdir -p ${TARGET_DIR}/lib/firmware
 cp -ar ${WIFI_FIRMWARE} ${TARGET_DIR}/lib/firmware
+cp -ar ${WIFI_SCRIPT} ${TARGET_DIR}/etc/init.d
 
