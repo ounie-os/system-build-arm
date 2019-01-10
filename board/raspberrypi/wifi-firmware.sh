d82c44e3355cfbf58f7a8cc425d187050be4686b
ounie_os <ounie@qq.com>
2019-01-10 10:44:12 +0800

    网口配置为静态ip

--- board/raspberrypi/wifi-firmware.sh
+++ board/raspberrypi/wifi-firmware.sh
@@ -5,7 +5,7 @@ set -e
 
 BOARD_DIR="$(dirname $0)"
 WIFI_FIRMWARE="${BOARD_DIR}/brcm"
-WIFI_SCRIPT="${BOARD_DIR}/S99wifi-module"
+WIFI_SCRIPT="${BOARD_DIR}/S99utils"
 
 mkdir -p ${TARGET_DIR}/lib/firmware
 cp -ar ${WIFI_FIRMWARE} ${TARGET_DIR}/lib/firmware
