cb1dcb99e5f31b7c0c2a2fbd6d30f63bf174c390
ounie_os <ounie@qq.com>
2019-07-24 14:52:51 +0800

    默认脚本变为S97xdp

--- board/raspberrypi/wifi-firmware.sh
+++ board/raspberrypi/wifi-firmware.sh
@@ -5,7 +5,7 @@ set -e
 
 BOARD_DIR="$(dirname $0)"
 WIFI_FIRMWARE="${BOARD_DIR}/brcm"
-WIFI_SCRIPT="${BOARD_DIR}/S99utils"
+WIFI_SCRIPT="${BOARD_DIR}/S97xdp"
 
 mkdir -p ${TARGET_DIR}/lib/firmware
 cp -ar ${WIFI_FIRMWARE} ${TARGET_DIR}/lib/firmware
